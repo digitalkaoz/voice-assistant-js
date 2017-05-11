25b4c16f2a42dc2511c53bc851ff71924ce8432c
Robert Schönthal <robert.schoenthal@gmail.com>
2017-05-11 13:03:40 +0200

    build cleanup

--- tests/function/LambdaFunctionSpec.ts
+++ tests/function/LambdaFunctionSpec.ts
@@ -1,4 +1,4 @@
-import {Container} from '../../src/Container'
+import {Container, lambda} from '../../src/Container'
 import {LamdaFunction} from '../../src/function/LamdaFunction'
 import {AutoDetectHandler} from '../../src/handler/AutoDetectHandler'
 
@@ -31,4 +31,23 @@ describe('LamdaFunction', () => {
 
     c.close()
   })
+
+  /*it('works with the shorthand function', () => {
+    const callback = jest.fn()
+    context.succeed = callback
+
+    lambda(mapping)(event, context, callback)
+
+    expect(callback).toHaveBeenCalledWith({
+      response: {
+        outputSpeech: {
+          ssml: '<speak> foo </speak>',
+          type: 'SSML'
+        },
+        shouldEndSession: true
+      },
+      sessionAttributes: {},
+      version: '1.0'
+    })
+  })*/
 })
