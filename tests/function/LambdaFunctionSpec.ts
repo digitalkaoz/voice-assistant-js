179ea8d0e4ec511c259c8524072376d83ca92515
Robert Schönthal <robert.schoenthal@gmail.com>
2017-05-15 17:05:22 +0200

    test all handlers from Function specs

--- tests/function/LambdaFunctionSpec.ts
+++ tests/function/LambdaFunctionSpec.ts
@@ -1,33 +1,14 @@
-import {Container, lambda} from '../../src/Container'
+import {lambda} from '../../src/Container'
 import {LamdaFunction} from '../../src/function/LamdaFunction'
 
 import mapping from '../fixtures/mapping'
 
-const event = require('../fixtures/alexa/event.json')
-const context = require('../fixtures/alexa/context.json')
-
 describe('LamdaFunction', () => {
-  it('calls the given IApi', () => {
-    const lamda = new LamdaFunction(Container.create(mapping))
-    const callback = jest.fn()
-    context.succeed = callback
 
-    lamda.invoke(event, context, callback)
+  it('can process alexa requests', () => {
+    const event = require('../fixtures/alexa/event.json')
+    const context = require('../fixtures/alexa/context.json')
 
-    expect(callback).toHaveBeenCalledWith({
-      response: {
-        outputSpeech: {
-          ssml: '<speak> foo </speak>',
-          type: 'SSML'
-        },
-        shouldEndSession: true
-      },
-      sessionAttributes: {},
-      version: '1.0'
-    })
-  })
-
-  it('works with the shorthand function', () => {
     const callback = jest.fn()
     context.succeed = callback
 
@@ -45,4 +26,36 @@ describe('LamdaFunction', () => {
       version: '1.0'
     })
   })
+
+  it('can process api-api requests', () => {
+    const event = require('../fixtures/apiai/event.json')
+    const callback = jest.fn()
+    const context = {
+      succeed: callback
+    }
+
+    lambda(mapping)(event, context, callback)
+
+    expect(callback).toHaveBeenCalledWith(undefined, {
+      contextOut: [],
+      data: {google: {expect_user_response: false, is_ssml: false, no_input_prompts: []}},
+      speech: 'default'
+    })
+  })
+
+  it('can process google-action requests', () => {
+    const event = require('../fixtures/google-action/event.json')
+    const callback = jest.fn()
+    const context = {
+      succeed: callback
+    }
+
+    lambda(mapping)(event, context, callback)
+
+    expect(callback).toHaveBeenCalledWith(undefined, {
+      expect_user_response: false,
+      final_response: {speech_response: {text_to_speech: 'default'}}
+    })
+  })
+
 })
